cbbfabe0f9cd303f82cd3d5da0a0b07f99455994
Palm <palml1lax@gmail.com>
2024-01-11 16:04:30 +0700

    feat:finish ex

--- exercise-7.js
+++ exercise-7.js
@@ -1,1 +1,13 @@
 // Paste the complete MQL here
+
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { year: { $year: "$created_at" }, month: { $month: "$created_at" } },
+      total_price_per_month: { $sum: "$total_price" },
+    },
+  },
+  { $match: { "_id.year": 2021, "_id.month": 7 } },
+  { $sort: { total_price_per_month: 1 } },
+  { $limit: 1 },
+]);
